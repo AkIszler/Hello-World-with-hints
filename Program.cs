8916ae164d0b101cfee2b534d8d4dbe06bbe3f19
AkIszler <114718803+AkIszler@users.noreply.github.com>
2022-11-01 00:55:12 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Buffers;
+using System.Data;
+using System.Net.Security;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        // Entry point of program 
+        static void Main(string[] args)
+        {
+            // Write line - Print to Console
+            // Read line - keep window up
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Sono");
+            Console.ReadLine();
+        }
+    }
+}
